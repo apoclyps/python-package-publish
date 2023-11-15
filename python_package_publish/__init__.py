7a2c6c81a8e3fe0f5f105385edb4867868ec339a
Kyle Harrison <kyle90adam@hotmail.com>
2023-11-15 13:48:25 +0000

    test: implements freezegun to enable freezing of time for testing (#12)

--- python_package_publish/__init__.py
+++ python_package_publish/__init__.py
@@ -1,4 +1,5 @@
 from python_package_publish.collections import chunk
+from python_package_publish.utils import get_utc_now, get_uuid
 from python_package_publish.version import __version__  # NOQA: F401
 
-__all__ = ["chunk"]
+__all__ = ["chunk", "get_uuid", "get_utc_now"]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,25 @@
-import datetime
+from datetime import UTC, datetime
 from uuid import UUID
 
-from python_package_publish.utils import get_utc_now, get_uuid
+import pytest
+from freezegun import freeze_time
+
+from python_package_publish.utils import get_utc_now
 
 
-def test_get_uuid() -> None:
+def test_get_uuid_is_valid_uuid() -> None:
     """Test that get_uuid() returns a valid uuid."""
-    UUID(get_uuid())
+    try:
+        UUID("")
+    except ValueError:
+        pytest.fail("get_uuid() did not return a valid uuid.")
 
 
+@freeze_time("1955-11-12 06:38:00")
 def test_get_utc_now() -> None:
     """Test that get_utc_now() returns the current utc datetime."""
-    now = get_utc_now()
-    assert now.tzinfo == datetime.UTC
+    now: datetime = get_utc_now()
+
+    assert now == datetime(
+        year=1955, month=11, day=12, hour=6, minute=38, second=0, tzinfo=UTC
+    )
