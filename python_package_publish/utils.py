adb5d418ebfae1a42b56fccd7f25f6ba59544ad0
Chris McEvoy <12284065+chrisimcevoy@users.noreply.github.com>
2023-11-15 10:43:49 +0000

    feat(utils): add utils.py

---
+++ python_package_publish/utils.py
@@ -0,0 +1,20 @@
+import datetime as _datetime
+import uuid as _uuid
+
+
+def get_uuid() -> str:
+    """Produces a string uuid4 when provided as the default value within
+    a SQLAlchemy model. Also used to produce deterministic UUIDs when
+    generation new models during testing.
+    """
+    return str(_uuid.uuid4())
+
+
+def get_utc_now() -> _datetime.datetime:
+    """Produces the current datetime as UTC when provided as a default within
+    a SQLAlchemy model. Also used to freeze datetime via freezegun during testing.
+    """
+    return _datetime.datetime.now(_datetime.UTC)
+
+
+__all__ = ["get_uuid", "get_utc_now"]
